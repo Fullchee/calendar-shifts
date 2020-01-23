b3313a85801191ef2a32092ad926cf0f6d7ddb4c
Fullchee Zhang <fullchee@gmail.com>
2020-01-23 14:01:17 -0500

    Calendar can add events

---
+++ src/components/Calendar.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ApiCalendar from "react-google-calendar-api";
+
+export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleItemClick = this.handleItemClick.bind(this);
+  }
+
+  listEvents = () => {
+    if (ApiCalendar.sign)
+      ApiCalendar.listUpcomingEvents(10).then(({ result }) => {
+        console.log(result.items);
+      });
+  };
+
+  handleItemClick = (event, name) => {
+    if (name === "sign-in") {
+      ApiCalendar.handleAuthClick();
+    } else if (name === "sign-out") {
+      ApiCalendar.handleSignoutClick();
+    }
+  };
+
+  createEvent = e => {
+    const eventFromNow = {
+      summary: "Poc Dev From Now",
+      time: 480
+    };
+
+    ApiCalendar.createEventFromNow(eventFromNow)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={e => this.handleItemClick(e, "sign-in")}>
+          sign-in
+        </button>
+        <button onClick={e => this.handleItemClick(e, "sign-out")}>
+          sign-out
+        </button>
+        <button onClick={this.createEvent}>Create event</button>
+      </>
+    );
+  }
+}
